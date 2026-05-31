bb27cd9ec6b9a7db62f1192d37e82a2b997b8829
Land-Light <12satorei3100@gmail.com>
2026-05-31 17:25:07 +0900

    Add Supabase Data API grants

---
+++ supabase-data-api-grants.sql
@@ -0,0 +1,5 @@
+grant usage on schema public to anon, authenticated;
+
+grant select, insert, update, delete on table inventory_rooms to anon, authenticated;
+grant select, insert, update, delete on table inventory_products to anon, authenticated;
+grant select, insert, update, delete on table inventory_count_entries to anon, authenticated;
